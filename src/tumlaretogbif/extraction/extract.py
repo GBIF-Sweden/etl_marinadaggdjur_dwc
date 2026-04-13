a13202c18e1893261ee620a816d1277a43567afc
shahmanash <manash.shah@gmail.com>
2026-04-13 11:38:00 +0200

    add fetch_data_in_chunks function

--- src/tumlaretogbif/extraction/extract.py
+++ src/tumlaretogbif/extraction/extract.py
@@ -23,3 +23,19 @@ def create_connection(db_credentials, db_config):
     except Exception as e:
         logging.error(f"An error occurred while connecting to the database: {e}")
         raise
+
+
+# Function to execute a SELECT query and yield data in chunks
+def fetch_data_in_chunks(engine, query, batch_size, dtypes_dict):
+    try:
+        with engine.connect() as connection:
+            for chunk in pd.read_sql_query(
+                query, connection, chunksize=batch_size, dtype=dtypes_dict
+            ):
+                chunk = chunk.astype(dtypes_dict)
+                # Filter out empty or all-NA chunks
+                if not chunk.empty and not chunk.isna().all().all():
+                    yield chunk
+    except Exception as e:
+        logging.error(f"An error occurred while fetching data: {e}")
+        raise
