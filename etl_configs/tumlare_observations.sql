d63e3d962faab53d5934b7fde3d46d7babf55d17
shahmanash <manash.shah@gmail.com>
2026-04-13 15:48:00 +0200

    add tumlare_observations.sql query

---
+++ etl_configs/tumlare_observations.sql
@@ -0,0 +1,32 @@
+SELECT
+    observations.id,
+    observation_date,
+    animal_condition,
+    time_start,
+    time_end,
+    latitude,
+    longitude,
+    distance_min,
+    distance_max,
+    quantity_min,
+    quantity_max,
+    quantity_cubs_min,
+    quantity_cubs_max,
+    quantity_dead,
+    dead_length_min,
+    dead_length_max,
+    observer_comment,
+    observations.updated_at,
+    counties.name,
+    animal_decomposion,
+    (SELECT GROUP_CONCAT(filename SEPARATOR ',')
+        FROM images
+        WHERE public IS TRUE AND observation = observations.id) AS associatedMedia
+FROM
+    observations
+LEFT JOIN
+    counties
+ON
+    observations.county = counties.id
+WHERE
+    observations.record_approved = TRUE
